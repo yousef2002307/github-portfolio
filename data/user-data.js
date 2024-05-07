8287683f3ec6cce1e41f7d1118a9cbade20ce75d
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2024-05-07 16:41:25 +0300

    First Commit

---
+++ data/user-data.js
@@ -0,0 +1,13 @@
+export const userData = {
+  githubUser: 'yousef2002307',
+  devUsername: "yousef",
+  github: 'https://github.com/yousef2002307',
+  facebook: 'https://www.facebook.com/profile.php?id=100085657487681',
+  linkedIn: 'https://www.linkedin.com/in/yousef-ahmed-4b9a4b251',
+  twitter: '',
+  stackOverflow: 'https://stackoverflow.com/users/24914143/user24914143',
+  leetcode: "https://leetcode.com/u/mmnwat6/",
+  resume: "https://drive.google.com/drive/folders/19QTPYzVKxCu8ptV9ctfPWklrl_sZqmoj?usp=sharing",
+  skills: ['php','javascript','React','python', 'NextJS', 'Redux', 'laravel', 'MySql', 'restful api', 'css', 'html', 'bootstrap','jquery','sass','pug.js','gulp'],
+  timezone: '+3'
+};
